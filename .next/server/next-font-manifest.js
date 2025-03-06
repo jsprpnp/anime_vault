19f8df8ab1ca29a21d0ffd7e3411a91be9625b3b
Jas <jasper.panopio@gmail.com>
2025-03-06 23:25:05 +0800

    Fetch with Framer Motion

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/jasperpanopio/Documents/anime_vault/app/layout\":[\"static/media/7e6a2e30184bb114-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
